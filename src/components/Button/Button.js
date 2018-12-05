32afa224d7a7d499f9259d784679019138ee5972
Blueheim <crystalrod0@gmail.com>
2018-12-05 16:42:38 +0100

    progress

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { suffixes } from '../../data.js';
+import { suffixes } from '../data.js';
 
 const Button = props => {
   const roundedClasses = {};
